e6ed17ee46592b56db23857cf6b3556726c884e8
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-02-19 12:56:32 +0530

    created utils with OpenAI API

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import "dotenv/config";
 import cors from "cors";
 
 const app = express();
@@ -12,32 +11,4 @@ app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
 
-app.post("/test", async (req, res) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: "gpt-4o-mini",
-      messages: [
-        {
-          role: "user",
-          content: req.body.message,
-        },
-      ],
-    }),
-  };
-  try {
-    const response = await fetch(
-      "https://api.openai.com/v1/chat/completions",
-      options,
-    );
-    const data = await response.json();
-    console.log(data.choices[0].message.content);
-    res.send(data.choices[0].message.content);
-  } catch (error) {
-    console.log(err);
-  }
-});
+app.post("/test", async (req, res) => {});
