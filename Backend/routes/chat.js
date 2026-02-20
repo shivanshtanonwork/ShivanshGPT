733271f372d566dbbe38ebd4ada9b465196fae6a
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-02-20 11:14:55 +0530

    Created Backend

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Thread from "../models/Thread.js";
+import getOpenAIAPIResponse from "../utils/openai.js";
 
 const router = express.Router();
 
@@ -19,4 +20,74 @@ router.post("/test", async (req, res) => {
   }
 });
 
+//Get all threads
+router.get("/thread", async (req, res) => {
+  try {
+    const threads = await Thread.find({}).sort({ updatedAt: -1 });
+    // most recent data at top
+    res.json(threads);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Falied to fetch threads" });
+  }
+});
+
+router.get("/thread/:threadId", async (req, res) => {
+  const { threadId } = req.params;
+
+  try {
+    const thread = await Thread.findOne({ threadId });
+    if (!thread) {
+      res.status(404).json({ error: "Thread not found" });
+    }
+    res.json(thread.messages);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Falied to fetch Chat" });
+  }
+});
+
+router.delete("/thread/:threadId", async (req, res) => {
+  const { threadId } = req.params;
+  try {
+    const deletedThread = await Thread.findOneAndDelete({ threadId });
+    if (!deletedThread) {
+      res.status(404).json({ error: "Thread not found" });
+    }
+    res.status(200).json({ success: "Thread deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Falied to delete Thread" });
+  }
+});
+
+router.post("/chat", async (req, res) => {
+  const { threadId, message } = req.body;
+  if (!threadId || !message) {
+    res.status(400).json({ error: "missing required fields" });
+  }
+  try {
+    const thread = await Thread.findOne({ threadId });
+
+    if (!thread) {
+      //create a new thread in DB
+      thread = new Thread({
+        threadId,
+        title: message,
+        messages: [{ role: "user", content: message }],
+      });
+    } else {
+      thread.messages.push({ role: "user", content: message });
+    }
+
+    const assistantReply = await getOpenAIAPIResponse(message);
+    thread.messages.push({ role: "assistant", content: assistantReply });
+    thread.updatedAt = new Date();
+    await thread.save();
+    res.json({ reply: assistantReply });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "something went wrong" });
+  }
+});
 export default router;
